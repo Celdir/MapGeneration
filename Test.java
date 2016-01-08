f34167eeeb0768d75cde085d3e7b9c1c6f0f6a62
Celdir <michael.earl7769@gmail.com>
2016-01-08 01:15:12 -0600

    added minimal test code

--- Test.java
+++ Test.java
@@ -1,9 +1,15 @@
-import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
 import simplevoronoi.*;
 
 public class Test {
     public static void main(String[] args) {
-        Voronoi v = new Voronoi(0.00001f);
-        List<GraphEdge> allEdges = v.generateVoronoi();
+        double[] xVals = {1, 2, 3, 26, 7};
+        double[] yVals = {1, 2, 3, 5, 28};
+        Voronoi v = new Voronoi(1);
+        List<GraphEdge> allEdges = v.generateVoronoi(xVals, yVals, 0, 50, 0, 50);
+        for (GraphEdge e : allEdges) {
+            System.out.println("(" + e.x1 + ", " + e.y1 + ") to (" + e.x2 + ", " + e.y2 + ")");
+        }
     }
 }
